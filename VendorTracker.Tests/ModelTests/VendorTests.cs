5a71f1962e90530f1ea474b76c6463c15befdc43
Dana <dcluukko@gmail.com>
2020-03-13 11:16:13 -0700

    show unable to test instanciation of Vendor constructor with name property without makeing name property

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -13,10 +13,20 @@ namespace VendorTracker.TestTools
     public void VendorConstructor_CreatesInstanceofVendor_Vendor()
     {
       Vendor newVendor = new Vendor();
-
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-    }
-    
-  }
+    } 
 
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Petunias";
+      Vendor newVendor = new Vendor(name);
+      //Act
+      string compare = newVendor.Name;
+      //Assert
+      Assert.AreEqual(name, compare);
+
+    }
+  }
 }
